e7ad43dc664b1773f7849a915282b78648212dca
Timotej Gale <timotej.gale@gmail.com>
2020-01-05 00:09:36 +0100

    Fix image deletion and paths

--- image_upload/database/crud.py
+++ image_upload/database/crud.py
@@ -11,8 +11,10 @@ def create_image(db: Session, file_name: str, file_hash: str, user_id: int):
     db.refresh(db_image)
     return db_image
 
+
 def delete_image(db: Session, image_id: int):
     db_image = db.query(models.Image).filter(models.Image.id == image_id).first()
-    db.delete(db_image)
-    db.commit()
+    if db_image:
+        db.delete(db_image)
+        db.commit()
     return db_image
--- image_upload/routers/upload.py
+++ image_upload/routers/upload.py
@@ -31,7 +31,7 @@ bucket = storage_client.bucket(bucket_name)
 
 
 
-@router.post('/images/', response_model=schemas.Image)
+@router.post('/images', response_model=schemas.Image)
 def upload(*, user_id: int = Form(...), file: UploadFile = File(...), db: Session = Depends(get_db)):
     try:
         Image.open(file.file)
@@ -62,10 +62,10 @@ def upload(*, user_id: int = Form(...), file: UploadFile = File(...), db: Sessio
 
 @router.delete('/images/{image_id}', response_model=schemas.Image)
 def delete_image(image_id: int, db: Session = Depends(get_db)):
-    db_user = crud.delete_image(db=db, image_id=image_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail='User not found')
-    return db_user
+    db_image = crud.delete_image(db=db, image_id=image_id)
+    if db_image is None:
+        raise HTTPException(status_code=404, detail='Image not found')
+    return db_image
 
 
 @router.get('/settings')
